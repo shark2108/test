bc277d25f76cc8d3e6e815a35b08fd9af00b2a3e
sharyar <sulejmanovsarar@gmail.com>
2023-02-28 13:01:42 +0500

    First save

---
+++ testdev3/testdev3.1.py
@@ -0,0 +1,18 @@
+class priamougol:
+    def pmk(self, perimetr , ploshad):
+        self.P = perimetr
+        self.S = ploshad
+        return self.P , self.S
+    
+    def __init__(self,a+b+c, a*b)
+    self.P = a+b+c
+    self.S = a*b
+    return self.P , self.S
+
+    @staticmethod
+    def odinak (chislo):
+        if  chislo > 1:
+            return True
+        
+
+        
---
+++ testdev3/testdev3.py
@@ -0,0 +1,8 @@
+class kvadrat:
+    def kvt(self, perimetr , ploshad) :
+        self.P = perimetr * 4
+        self.S = a * 4
+        return self.P, self.S
+    
+
+
